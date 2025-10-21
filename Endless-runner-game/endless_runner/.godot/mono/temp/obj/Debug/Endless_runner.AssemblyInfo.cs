f449124d8e1fc43c54a35dfa4e261250af34aafc
me mee <iitaellu@hotmail.com>
2025-10-21 18:09:18 +0300

    .
    .

--- Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
+++ Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2dcc3d11877188640ec15a4e9ee2809a1824b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00299591bb670a6036408942e719092d4fc5eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
