6f12d0d7f1ff78ecc976e0cd887999cd3d6feeb8
me mee <iitaellu@hotmail.com>
2025-11-09 15:30:26 +0200

    graphic update
    Added new graphics for buttons, collectables and finish line

--- Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
+++ Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9fd28ccc917f46631482c87b90084c37fc2f35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb702adb795c816f6439c5e9fd85a8d4e289b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
