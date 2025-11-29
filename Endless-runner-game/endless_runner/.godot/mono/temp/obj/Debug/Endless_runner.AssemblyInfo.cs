a9849c0d6e838ec412face5c1b6eb791a057f404
me mee <iitaellu@hotmail.com>
2025-11-29 17:48:56 +0200

    Finish back ground added
    Finish back ground added

--- Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
+++ Endless-runner-game/endless_runner/.godot/mono/temp/obj/Debug/Endless_runner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf892862cfedeaa56a3d04228a07edead9a3c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8cf58bdca79a3d3382c150996a0060eebffefb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Endless_runner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
